49f36c4761b88cdae2659a3d8a86f0a6ca374d08
cristian <20201032@uaz.edu.mx>
2024-09-30 23:58:20 -0600

    Variables

--- hello_world.go
+++ hello_world.go
@@ -1,9 +1,41 @@
 package main
 
-// paquete de formateo 
-import "fmt"
+// paquete de formateo
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
 	fmt.Println("Hello World! GO")
-}
 
+	/*
+		Comentarios de varias lineas
+	*/
+
+	// Variables
+	// Se puede inferir el tipo de dato
+	var myString string = "Mi String"
+	fmt.Println(myString)
+
+	myString = "Mi String Modificada"
+	fmt.Println(myString)
+
+	/*
+		Si la variable se define con un tipo de dato,
+		no se puede cambiar el tipo de dato de la variable
+	*/
+
+	// Por defecto un int es 32 bits
+	var myInt int = 10
+	fmt.Println("Mi int es:", myInt)
+	fmt.Println("Mi int + 1:", myInt + 1) 
+	fmt.Println("Mi int:", myInt)
+
+	// Para un numero de 64 bits
+	var myInt64 int64 = 1000000000000000000
+	fmt.Println("Mi int64 es:", myInt64)
+
+	fmt.Printf("%s %d", myString ,myInt) 
+
+}
